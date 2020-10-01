1e5619c6063b4e66b6cfb836c7688d21541834a0
Anton Hristozov <adhristozov@sei.cmu.edu>
2020-10-01 16:04:28 -0400

    Added an example for simple memory attack through /proc

---
+++ simulated-attacks/mem-integrity/host_app.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int len(char *str);
+
+int main(int ac, char **av)
+{
+	char *str;
+	int i = 1;
+
+	if (ac == 2)
+	{
+		str = malloc(len(av[1]) + 1);
+		strcpy(str, av[1]);
+	}
+	while (i)
+	{
+		printf("[%d] %s - addr: %p\n", i, str, str);
+		sleep(1);
+		i++;
+	}
+	free(str);
+	return (0);
+}
+
+int len(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
