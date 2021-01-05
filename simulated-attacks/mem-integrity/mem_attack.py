bb084a9f4db8f3b3733c7965e3bcef18a8c21a94
antonhristozov <anton.hristozov@gmail.com>
2021-01-05 17:45:01 +0000

    Removed heap dependencies

--- simulated-attacks/mem-integrity/mem_attack.py
+++ simulated-attacks/mem-integrity/mem_attack.py
@@ -7,40 +7,17 @@ from sys import argv, exit
 def print_usage():
 	"""Print the usage string if script was used improperly"""
 	print('Usage: \
-		\t$ {} <pid> <heap_addr from heap start> <byte0> <byte1> <byte2> <byte3> <byte4>'.format(argv[0]))
+		\t$ {} <pid> <addr > <byte0> <byte1> <byte2> <byte3> <byte4>'.format(argv[0]))
 	print("e.g. : \t$ python3 mem_attack.py 21566 0x64ed88 0 0 0 0 0")
 	exit(1)
 
 
-def read_write_heap(pid, offset, byte0, byte1, byte2, byte3, byte4):
-	"""Find @read_str in the heap of @pid and replace it with @write_str"""
-	try:
-		maps_file = open("/proc/{}/maps".format(pid), 'r')
-	except IOError as e:
-		print("Can't open file /proc/{}/maps: IOError: {}".format(pid, e))
-		exit(1)
-	heap_info = None
-	for line in maps_file:
-		if 'heap' in line:
-			heap_info = line.split()
-	maps_file.close()
-	if 'heap' == None:
-		print('No heap found!')
-		exit(1)
-	addr = heap_info[0].split('-')
-	perms = heap_info[1]
-	if 'r' not in perms or 'w' not in perms:
-		print('Heap does not have read and/or write permission')
-		exit(0)
+def read_write_mem(pid, offset, byte0, byte1, byte2, byte3, byte4):
 	try:
 		mem_file = open("/proc/{}/mem".format(pid), 'rb+')
 	except IOError as e:
 		print("Can't open file /proc/{}/maps: IOError: {}".format(pid, e))
 		exit(1)
-	heap_start = int(addr[0], 16)
-	heap_end = int(addr[1], 16)
-	mem_file.seek(heap_start)
-	heap = mem_file.read(heap_end - heap_start)
 	int_offset = int(offset,16)
 	print(hex(int_offset))
 	while True:
@@ -63,6 +40,6 @@ if (len(argv) == 8):
 	b2 = int(argv[5])
 	b3 = int(argv[6])
 	b4 = int(argv[7])
-	read_write_heap(pid, offset, b0, b1, b2, b3, b4)
+	read_write_mem(pid, offset, b0, b1, b2, b3, b4)
 else:
 	print_usage()
